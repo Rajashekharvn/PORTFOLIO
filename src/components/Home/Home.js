913511d9c9ecef5fb90f8d35df6709a57f2f9dbb
Rajashekhar Naduvinahalli <103878269+Rajashekharvn@users.noreply.github.com>
2025-11-13 18:02:34 +0530

    Update Home.js
    removed N in name

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -23,7 +23,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> RAJASHEKHAR N</strong>
+                <strong className="main-name"> RAJASHEKHAR</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
