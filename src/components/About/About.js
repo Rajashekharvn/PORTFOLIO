d632b745ddaf5043158b3fe1a99458448eed1669
Rajashekhar Naduvinahalli <103878269+Rajashekharvn@users.noreply.github.com>
2025-11-13 22:02:33 +0530

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,77 +9,70 @@ import Toolstack from "./Toolstack";
 import "./About.css";
 
 function About() {
+  // Intersection Observer to trigger reveal animations
   useEffect(() => {
-    const revealEls = document.querySelectorAll(".reveal");
+    const revealElements = document.querySelectorAll(
+      ".reveal, .reveal-left, .reveal-right, .reveal-child, .stagger > *"
+    );
 
     const observer = new IntersectionObserver(
-      (entries, obs) => {
+      (entries) => {
         entries.forEach((entry) => {
-          if (!entry.isIntersecting) return;
-
-          const el = entry.target;
-
-          // Stagger children
-          if (el.classList.contains("stagger")) {
-            [...el.children].forEach((child, i) => {
-              child.style.transitionDelay = `${i * 70}ms`;
-              child.classList.add("is-visible");
-            });
+          if (entry.isIntersecting) {
+            entry.target.classList.add("is-visible");
           }
-
-          // Reveal element itself
-          el.classList.add("is-visible");
-          obs.unobserve(el);
         });
       },
       { threshold: 0.15 }
     );
 
-    revealEls.forEach((el) => observer.observe(el));
-    return () => revealEls.forEach((el) => observer.unobserve(el));
+    revealElements.forEach((el) => observer.observe(el));
   }, []);
 
   return (
     <Container fluid className="about-section">
       <Particle />
-      <Container>
 
-        {/* About Row */}
-        <Row className="reveal reveal-left" style={{ justifyContent: "center", padding: "10px" }}>
-          <Col md={7} style={{ paddingTop: "30px", paddingBottom: "50px" }}>
-            <h1 className="project-heading">
+      <Container>
+        {/* Heading */}
+        <Row style={{ justifyContent: "center", padding: "10px" }}>
+          <Col md={7} className="reveal-left">
+            <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
               Know Who <strong className="purple">I'M</strong>
             </h1>
-            <div className="reveal stagger">
+
+            {/* About Card */}
+            <div className="reveal-left">
               <Aboutcard />
             </div>
           </Col>
 
-          <Col md={5} className="about-img reveal reveal-right" style={{ paddingTop: "120px", paddingBottom: "50px" }}>
-            <img src={laptopImg} alt="about" className="img-fluid float-animation reveal-child" />
+          {/* Image */}
+          <Col md={5} className="about-img reveal-right">
+            <img
+              src={laptopImg}
+              alt="about"
+              className="img-fluid float-animation"
+            />
           </Col>
         </Row>
 
-        <h1 className="project-heading reveal">
-          Professional <strong className="purple">Skillset</strong>
-        </h1>
-
-        <div className="reveal stagger">
+        {/* Skillset */}
+        <h1 className="project-heading reveal">Professional <strong className="purple">Skillset</strong></h1>
+        <div className="stagger">
           <Techstack />
         </div>
 
-        <h1 className="project-heading reveal">
-          <strong className="purple">Tools</strong> I use
-        </h1>
-
-        <div className="reveal stagger">
+        {/* Tools */}
+        <h1 className="project-heading reveal"><strong className="purple">Tools</strong> I use</h1>
+        <div className="stagger">
           <Toolstack />
         </div>
 
+        {/* Github snake animation */}
         <div className="reveal">
           <Github />
         </div>
-
       </Container>
     </Container>
   );
