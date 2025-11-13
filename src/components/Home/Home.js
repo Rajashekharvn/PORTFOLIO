5e7b90bb210590b5510baf3952d3399a294dbce4
Rajashekhar Naduvinahalli <103878269+Rajashekharvn@users.noreply.github.com>
2025-11-13 18:05:08 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -23,7 +23,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> RAJASHEKHAR</strong>
+                <strong className="main-name"> RAJASHEKHAR V N</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
