fccf57da2690d03be410a552a914557bd55f53f1
Rajashekhar Naduvinahalli <103878269+Rajashekharvn@users.noreply.github.com>
2025-11-13 20:13:40 +0530

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,41 +1,19 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { FaAws, FaCode } from "react-icons/fa"; // Using AWS and a generic code icon
+import { FaAws, FaCode } from "react-icons/fa";
 import { DiJavascript1, DiPython, DiGit } from "react-icons/di";
 import { SiHtml5, SiCss3 } from "react-icons/si";
-
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons" data-name="JavaScript">
-  <DiJavascript1 />
-</Col>
-
-<Col xs={4} md={2} className="tech-icons" data-name="HTML">
-  <SiHtml5 />
-</Col>
-
-<Col xs={4} md={2} className="tech-icons" data-name="CSS">
-  <SiCss3 />
-</Col>
-
-<Col xs={4} md={2} className="tech-icons" data-name="Python">
-  <DiPython />
-</Col>
-
-<Col xs={4} md={2} className="tech-icons" data-name="Data Structures">
-  <FaCode />
-</Col>
-
-<Col xs={4} md={2} className="tech-icons" data-name="Git">
-  <DiGit />
-</Col>
-
-<Col xs={4} md={2} className="tech-icons" data-name="AWS">
-  <FaAws />
-</Col>
-
+      <Col className="tech-icons" data-name="JavaScript"><DiJavascript1 /></Col>
+      <Col className="tech-icons" data-name="HTML5"><SiHtml5 /></Col>
+      <Col className="tech-icons" data-name="CSS3"><SiCss3 /></Col>
+      <Col className="tech-icons" data-name="Python"><DiPython /></Col>
+      <Col className="tech-icons" data-name="Data Structures"><FaCode /></Col>
+      <Col className="tech-icons" data-name="Git"><DiGit /></Col>
+      <Col className="tech-icons" data-name="AWS"><FaAws /></Col>
     </Row>
   );
 }
