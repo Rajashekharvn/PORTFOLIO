42d2af0b110b81d227071f8304c26fa8a09510c3
Rajashekhar Naduvinahalli <103878269+Rajashekharvn@users.noreply.github.com>
2025-11-14 11:40:10 +0530

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,20 +1,27 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
 import { FaAws, FaCode } from "react-icons/fa";
 import { DiJavascript1, DiPython, DiGit } from "react-icons/di";
 import { SiHtml5, SiCss3 } from "react-icons/si";
 
 function Techstack() {
+  const items = [
+    { Icon: DiJavascript1, name: "JavaScript" },
+    { Icon: SiHtml5, name: "HTML5" },
+    { Icon: SiCss3, name: "CSS3" },
+    { Icon: DiPython, name: "Python" },
+    { Icon: FaCode, name: "DSA" },
+    { Icon: DiGit, name: "Git" },
+    { Icon: FaAws, name: "AWS" },
+  ];
+
   return (
-    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col className="tech-icons" data-name="JavaScript"><DiJavascript1 /></Col>
-      <Col className="tech-icons" data-name="HTML5"><SiHtml5 /></Col>
-      <Col className="tech-icons" data-name="CSS3"><SiCss3 /></Col>
-      <Col className="tech-icons" data-name="Python"><DiPython /></Col>
-      <Col className="tech-icons" data-name="Data Structures"><FaCode /></Col>
-      <Col className="tech-icons" data-name="Git"><DiGit /></Col>
-      <Col className="tech-icons" data-name="AWS"><FaAws /></Col>
-    </Row>
+    <>
+      {items.map(({ Icon, name }) => (
+        <div key={name} className="tech-icons" data-name={name}>
+          <Icon />
+        </div>
+      ))}
+    </>
   );
 }
 
