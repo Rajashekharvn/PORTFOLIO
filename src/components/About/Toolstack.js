75a766e70b8ffe891cf7c73a1c812463a7e3ff94
Rajashekhar Naduvinahalli <103878269+Rajashekharvn@users.noreply.github.com>
2025-11-14 12:54:41 +0530

    Update Toolstack.js

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,22 +1,31 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { SiVisualstudiocode, SiVercel, SiWindows, SiNetlify } from "react-icons/si";
+import {
+  SiVisualstudiocode,
+  SiVercel,
+  SiWindows,
+  SiNetlify,
+} from "react-icons/si"; // Selected tools
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiWindows />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVisualstudiocode />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVercel />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiNetlify />
-      </Col>
+      <Col xs={4} md={2} className="tech-icons" data-name="Windows">
+  <SiWindows />
+</Col>
+
+<Col xs={4} md={2} className="tech-icons" data-name="VS Code">
+  <SiVisualstudiocode />
+</Col>
+
+<Col xs={4} md={2} className="tech-icons" data-name="Vercel">
+  <SiVercel />
+</Col>
+
+<Col xs={4} md={2} className="tech-icons" data-name="Netlify">
+  <SiNetlify />
+</Col>
+
     </Row>
   );
 }
