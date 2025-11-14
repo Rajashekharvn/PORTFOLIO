0b8c4ccfda4f62f04708ef786ca8d8b95256fa9c
Rajashekhar Naduvinahalli <103878269+Rajashekharvn@users.noreply.github.com>
2025-11-14 12:33:48 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -4,36 +4,36 @@ import { ImPointRight } from "react-icons/im";
 
 function AboutCard() {
   return (
-    <Card className="quote-card-view reveal-child">
+    <Card className="quote-card-view">
       <Card.Body>
-        <blockquote className="blockquote mb-0 stagger">
-          <p className="stagger-child" style={{ textAlign: "justify" }}>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Rajashekhar N </span>
             from <span className="purple"> Karnataka, India.</span>
             <br />
-            I am currently pursuing my engineering in Computer Science &
-            Engineering at SKSVMACET Laxmeshwar.
+            I am currently pursuing my engineering in Computer Science & engineering at SKSVMACET Laxmeshwar.
+            <br />
+            
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
-
           <ul>
-            <li className="about-activity stagger-child">
+            <li className="about-activity">
               <ImPointRight /> Playing Chess
             </li>
-            <li className="about-activity stagger-child">
+            <li className="about-activity">
               <ImPointRight /> Watching Movies and Series
             </li>
-            <li className="about-activity stagger-child">
+            <li className="about-activity">
               <ImPointRight /> Travelling
             </li>
           </ul>
 
-          <p className="stagger-child" style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer stagger-child">Rajashekhar</footer>
+          <footer className="blockquote-footer">Rajashekhar</footer>
         </blockquote>
       </Card.Body>
     </Card>
